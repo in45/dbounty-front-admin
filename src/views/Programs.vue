dc1b278d32d3428e6cc6a0014ba2180bc90cdbef
inas180 <37088665+inas180@users.noreply.github.com>
2021-05-21 17:27:53 +0100

    - add report messages and edit reports page

--- src/views/Programs.vue
+++ src/views/Programs.vue
@@ -2,113 +2,130 @@
     <main>
         <div class="row mt-5 m-0">
             <div class="col-xl-11 ">
+                <div class="searchBox float-right">
+                    <input class="searchInput" type="text" name="" placeholder="Search">
+                    <button class="searchButton" href="#">
+                        <i class="fa fa-search">
+                        </i>
+                    </button>
+                </div>
+                <h2 class="mb-4">Programs</h2>
+                <div id="aspect-content">
+                    <div class="aspect-tab prio " v-for="data of programs" :key="data.id">
+                        <input :id="data.id" type="checkbox" class="aspect-input" name="aspect">
+                        <label :for="data.id" class="aspect-label"></label>
+                        <div class="aspect-content">
+                            <div class="col-xl-6">
+                                <b-avatar class="mr-2"></b-avatar>
+                                <span class="aspect-name mr-2">  {{data.name}}</span>
+                                <b-badge pill class="p-1" variant="info">{{data.status}}</b-badge>
+                            </div>
+                            <div class="col-xl-6 m-auto text-right">
+                                <ul class="row  list-inline my-0 p-0">
+                                    <li class="col-3 p-0 ">
+                                        <i class="flaticon-bug text-primary mr-2" title="type"></i>{{data.reports_count}}
+                                    </li>
+                                    <li class="col-3 p-0 ">
+                                        <i class="flaticon-cyber-security-2 text-primary mr-2" title="type"></i>{{data.type}}
+                                    </li>
+                                    <li class="col-3 p-0 ">
+                                        <i class="flaticon-skull  text-primary mr-2" title="reports"></i>{{data.reports_count}}
+                                    </li>
+                                    <li class="col-3 p-0 ">
+                                        <i class="flaticon-hacker-2 text-primary mr-2" title="hunters"></i>{{data.users_count}}
+                                    </li>
 
 
-                    <div class="searchBox float-right">
-                        <input class="searchInput" type="text" name="" placeholder="Search">
-                        <button class="searchButton" href="#">
-                            <i class="fa fa-search">
-                            </i>
-                        </button>
-                    </div>
-                    <h2 class="mb-4">Programs</h2>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="aspect-tab-content ">
+                            <div class="row m-0 py-2">
+                                <div class="col-xl-5">
+                                    <div class="card">
+                                        <div class=" mb-0 card-title">
+                                            <div class="row m-0">
+                                                <h5 class="col-8 pt-3 pl-3">Info</h5>
+                                                <b-form-select class="col-4" v-model="data.status" :options="status"
+                                                               size="sm"></b-form-select>
 
-                    <div id="aspect-content">
-            <div class="aspect-tab prio "  v-for="data of programs" :key="data.id">
-                <input :id="data.id" type="checkbox" class="aspect-input" name="aspect">
-                <label :for="data.id" class="aspect-label"></label>
-                <div class="aspect-content">
-                    <div class="col-xl-6">
-                        <b-avatar class="mr-2"></b-avatar>
-                        <span class="aspect-name mr-2">  {{data.name}}</span>
-                        <b-badge pill class="p-1" variant="info">{{data.status}}</b-badge>
-                    </div>
-                    <div class="col-xl-6 m-auto text-right" >
-                        <ul class="row  list-inline my-0 p-0">
-                            <li class="col-3 p-0 ">
-                                <i class="flaticon-bug text-primary mr-2" title="type"></i>{{data.reports_count}}
-                            </li>
-                            <li class="col-3 p-0 ">
-                                <i class="flaticon-cyber-security-2 text-primary mr-2" title="type"></i>{{data.type}}
-                            </li>
-                            <li class="col-3 p-0 ">
-                                <i class="flaticon-skull  text-primary mr-2" title="reports"></i>{{data.reports_count}}
-                            </li>
-                            <li class="col-3 p-0 ">
-                                <i class="flaticon-hacker-2 text-primary mr-2" title="hunters"></i>{{data.users_count}}
-                            </li>
+                                            </div>
+                                        </div>
 
+                                        <div class="card-body">
+                                            <h6 class="mb-2">
+                                                <b-avatar :src="data.company.logo" class="mr-2"
+                                                          alt="Company"></b-avatar>
+                                                {{data.company.name}}
+                                            </h6>
+                                            <h6 class="mb-2"><span class="text-info">Begin At :</span> {{data.begin_at}}
+                                            </h6>
+                                            <h6 class="mb-2"><span class="text-info">Finish At :</span>
+                                                {{data.finish_at}}</h6>
+                                            <p>{{JSON.parse(data.description).info}}</p>
+                                            <b-badge class="float-right p-2" role="button"
+                                                     @click="$router.push({name:'ProgramReports',params:{id:data.id}})"
+                                                     pill v-if="data.reports_count" variant="info">reports
+                                            </b-badge>
+                                            <b-badge class="float-right p-2 mr-2" role="button"
+                                                     @click="$router.push({name:'ProfilProgram',params:{id:data.id}})"
+                                                     pill variant="info">view more
+                                            </b-badge>
 
-                        </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-xl-4">
+                                    <div class="card">
+                                        <h5 class="card-title mb-0 pt-3 pl-3">Scopes</h5>
+                                        <div class="card-body" v-html="data.scopes">
+                                            {{data.scopes}}
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-xl-3">
+                                    <div class="card">
+                                        <div class="card-title">
+                                            <span class="float-right">{{data.min_bounty+ ' - '+ data.max_bounty}} <i
+                                                    class="flaticon-ethereum-2 text-primary ml-1"
+                                                    style="font-size: 18px" title="bounty"></i></span>
+                                            <h5 class=" mb-0 pt-3 pl-3">Rewards </h5>
+                                        </div>
+                                        <div class="card-body">
+                                            <div class="row m-0">
+                                                <span class="col-4">P1</span>
+                                                <p class="col-7 p-0 text-center prio">
+                                                    {{JSON.parse(data.description).rewards.p1}}</p>
+                                            </div>
+                                            <div class="row m-0">
+                                                <span class="col-4">P2</span>
+                                                <p class="col-7 p-0 text-center prio">
+                                                    {{JSON.parse(data.description).rewards.p2}}</p>
+                                            </div>
+                                            <div class="row m-0">
+                                                <span class="col-4">P3</span>
+                                                <p class="col-7 p-0 text-center prio">
+                                                    {{JSON.parse(data.description).rewards.p3}}</p>
+                                            </div>
+                                            <div class="row m-0">
+                                                <span class="col-4">P4</span>
+                                                <p class="col-7 p-0 text-center prio">
+                                                    {{JSON.parse(data.description).rewards.p4}}</p>
+                                            </div>
+                                            <div class="row m-0">
+                                                <span class="col-4">P5</span>
+                                                <p class="col-7 p-0 text-center prio">
+                                                    {{JSON.parse(data.description).rewards.p5}}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
-                <div class="aspect-tab-content ">
-                    <div class="row m-0 py-2">
-                        <div class="col-xl-5">
-                            <div class="card">
-                                <div class=" mb-0 card-title">
-                                    <div class="row m-0">
-                                    <h5 class="col-8 pt-3 pl-3">Info</h5>
-                                    <b-form-select class="col-4"  v-model="data.status" :options="status" size="sm"></b-form-select>
-
-                                </div>
-                                </div>
-
-                                <div class="card-body">
-                                    <h6 class="mb-2"><b-avatar :src="data.company.logo" class="mr-2" alt="Company"></b-avatar> {{data.company.name}}</h6>
-                                    <h6 class="mb-2"><span class="text-info">Begin At :</span> {{data.begin_at}}</h6>
-                                    <h6 class="mb-2"><span class="text-info">Finish At :</span> {{data.finish_at}}</h6>
-                                    <p>{{JSON.parse(data.description).info}}</p>
-                                    <b-badge class="float-right p-2" role="button"  @click="$router.push({name:'ProgramReports',params:{id:data.id}})" pill v-if="data.reports_count" variant="info">reports</b-badge>
-                                    <b-badge class="float-right p-2 mr-2" role="button"  @click="$router.push({name:'ProfilProgram',params:{id:data.id}})" pill variant="info">view more</b-badge>
-
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-4">
-                            <div class="card">
-                                <h5 class="card-title mb-0 pt-3 pl-3">Scopes</h5>
-                                <div class="card-body" v-html="data.scopes">
-                                    {{data.scopes}}
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-xl-3">
-                            <div class="card">
-                                <div class="card-title">
-                                    <span class="float-right">{{data.min_bounty+ ' - '+ data.max_bounty}} <i class="flaticon-ethereum-2 text-primary ml-1" style="font-size: 18px" title="bounty"></i></span>
-                                    <h5 class=" mb-0 pt-3 pl-3">Rewards </h5>
-                                </div>
-                                    <div class="card-body">
-                                    <div class="row m-0">
-                                        <span class="col-4">P1</span>
-                                        <p class="col-7 p-0 text-center prio" >{{JSON.parse(data.description).rewards.p1}}</p>
-                                    </div>
-                                    <div class="row m-0">
-                                        <span class="col-4">P2</span>
-                                        <p class="col-7 p-0 text-center prio" >{{JSON.parse(data.description).rewards.p2}}</p>
-                                    </div>
-                                    <div class="row m-0">
-                                        <span class="col-4">P3</span>
-                                        <p class="col-7 p-0 text-center prio" >{{JSON.parse(data.description).rewards.p3}}</p>
-                                    </div>
-                                    <div class="row m-0">
-                                        <span class="col-4">P4</span>
-                                        <p class="col-7 p-0 text-center prio" >{{JSON.parse(data.description).rewards.p4}}</p>
-                                    </div>
-                                    <div class="row m-0">
-                                            <span class="col-4">P5</span>
-                                            <p class="col-7 p-0 text-center prio" >{{JSON.parse(data.description).rewards.p5}}</p>
-                                     </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </div>
                 <div class="text-center my-4">
                     <paginate :current_page=current_page :last_page_url=last_page_url
                               v-on:change-page="changePage"/>
@@ -122,34 +139,34 @@
 <script>
 
     import Paginate from "../components/pagination";
+
     export default {
         name: "Programs",
         components: {Paginate},
-        data(){
-            return{
+        data() {
+            return {
                 current_page: 1,
-                status:['none','new','open','closed'],
-                programs:[],
-                val:'new',
-                last_page_url:6,
+                status: ['none', 'new', 'open', 'closed'],
+                programs: [],
+                val: 'new',
+                last_page_url: 6,
 
             }
         },
-        created () {
-        this.loadPrograms(1);
-    },
+        created() {
+            this.loadPrograms(1);
+        },
 
 
-        methods:{
-            changePage(page){
+        methods: {
+            changePage(page) {
                 this.current_page = page;
                 this.loadPrograms(page);
             },
 
-            loadPrograms(page)
-            {
-                    this.$http
-                    .get('programs?page='+page)
+            loadPrograms(page) {
+                this.$http
+                    .get('programs?page=' + page)
                     .then(response => {
                         console.log(response.data)
                         this.programs = response.data.data;
@@ -161,17 +178,17 @@
                         console.log(error)
                     })
             }
-            }
         }
+    }
 
 </script>
 
-<style scoped  src="@/assets/style/prog.css"></style>
+<style scoped src="@/assets/style/prog.css"></style>
 <style scoped>
 
     .searchBox {
 
-        transform: translate(-2%,50%);
+        transform: translate(-2%, 50%);
         background: #2f3640;
         height: 40px;
         border-radius: 40px;
@@ -186,7 +203,7 @@
 
     .searchBox:hover > .searchButton {
         background: white;
-        color : #2f3640;
+        color: #2f3640;
     }
 
     .searchButton {
@@ -203,10 +220,10 @@
     }
 
     .searchInput {
-        border:none;
+        border: none;
         background: none;
-        outline:none;
-        float:left;
+        outline: none;
+        float: left;
         padding: 0;
         color: white;
         font-size: 16px;
--- src/views/Reports.vue
+++ src/views/Reports.vue
@@ -61,9 +61,12 @@
                         </div>
 
                         <div class="card-body">
+
                             <div class="row mx-0 mb-2">
                                 <b-badge style="font-size: 13px" class="p-3 mr-3"  role="button" v-b-toggle.report variant="dark">Edit Report</b-badge>
+                                <edit-report :selected_report="selected_report"/>
                                 <b-badge style="font-size: 13px" class="p-3" role="button" v-b-toggle.messages variant="dark">View Messages</b-badge>
+                                <report-messages/>
                                 <b-form-select class="float-right ml-auto" style="width: 110px" v-model="selected_report.status" :options="status"></b-form-select>
                             </div>
 
@@ -130,95 +133,6 @@
                                         </b-card>
                                     </div>
 
-                            <b-sidebar id="report" title="Edit Report" width="600px" bg-variant="dark" text-variant="light" right shadow>
-
-                                <div class="px-3 py-2">
-                                <form id="form" method="POST" >
-                                    <div class="form-group row my-1 mx-0">
-                                        <label class="pb-2 col-xl-4 m-auto"> Vulnerability Title </label>
-                                        <b-form-input class="col-xl-8" v-model="selected_report.vuln_name" type="text" placeholder="Add your vulnerability" ></b-form-input>
-                                    </div>
-                                    <div class="form-group row my-1 mx-0">
-                                        <label class="pb-2 col-xl-4 m-auto"> Target </label>
-                                        <b-form-select class="col-xl-8" v-model="selected_report.target" placeholder="Add your Target" :options="['a','b','c']"></b-form-select>
-                                    </div>
-                                    <div class="form-group row my-1 mx-0">
-                                        <label class="pb-2 col-xl-4 m-auto"> Vulnerability Category </label>
-                                        <b-form-input list="browsers" class="col-xl-8" name="browser" v-model="selected_report.vuln.category" id="browser"></b-form-input>
-                                        <datalist class="col-xl-8" id="browsers">
-                                            <option v-for="c in categories" :key="c.id" :value="c.title">{{c.title}}</option>
-                                        </datalist>
-
-                                    </div>
-                                    <div class="form-group  row my-1 mx-0">
-                                        <label class="pb-2 pl-3"> Vulnerability Details </label>
-                                        <vue-editor v-model="selected_report.vuln_details"></vue-editor>
-                                    </div>
-                                    <div class="form-group row my-1 mx-0">
-                                        <label class="pb-2 pl-3"> Validations Steps </label>
-                                        <vue-editor v-model="selected_report.validation_steps"></vue-editor>
-                                    </div>
-                                    <input type="submit" value="Save" class="bg-primary text-white float-right my-2"/>
-                                </form>
-                                </div>
-                            </b-sidebar>
-                            <b-sidebar id="messages" title="Messages" bg-variant="dark" text-variant="light" right shadow>
-                                <div class="px-3 py-2">
-                                    <div class="chat-message">
-                                        <ul class="chat">
-                                            <li class="left row mx-0 mb-2 ">
-                                                <div class="col-xl-3 p-0">
-                                                    <b-avatar  alt="User Avatar"></b-avatar>
-                                                </div>
-
-                                                <div class="chat-body col-xl-9 ">
-                                                    <div class="header p-2">
-                                                        <strong class="primary-font">John Doe</strong>
-                                                        <small class="pull-right text-muted"><i
-                                                                class="fa fa-clock-o"></i> 12 mins ago</small>
-                                                    </div>
-                                                    <p class="p-2">
-                                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                                    </p>
-                                                </div>
-                                            </li>
-                                            <li class="right mb-2">
-                                                <div class="chat-body ">
-                                                    <div class="header p-2">
-                                                        <strong class="primary-font">Sarah</strong>
-                                                        <small class="pull-right text-muted"><i
-                                                                class="fa fa-clock-o"></i> 13 mins ago</small>
-                                                    </div>
-                                                    <p class="p-2">
-                                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                                        Curabitur bibendum ornare dolor, quis ullamcorper ligula sodales
-                                                        at.
-                                                    </p>
-                                                </div>
-                                            </li>
-
-                                        </ul>
-                                    </div>
-
-                                    <div class="col-xl-12 cmt border-top pt-3">
-                                        <span  id="my_cmt" class="t form-control pr-5" role="textbox" style="border-radius: 30px" contenteditable></span>
-                                        <div class="chat-input-links">
-                                            <ul class="list-inline mb-0">
-                                                <li class="list-inline-item">
-                                                    <a v-b-tooltip.hover placement="top"
-                                                       title="Send" >
-                                                        <i class="fa fa-paper-plane"></i>
-                                                    </a>
-                                                </li>
-
-                                            </ul>
-                                        </div>
-
-
-                                    </div> </div>
-                            </b-sidebar>
-
-
 
 
 
@@ -231,14 +145,17 @@
 </template>
 
 <script>
-    import { VueEditor } from "vue2-editor";
+
     import simplebar from 'simplebar-vue';
     import 'simplebar/dist/simplebar.min.css';
     import { vuln_Category } from "../assets/vuln_category";
+    import EditReport from "@/components/EditReport";
+    import ReportMessages from "@/components/ReportMessages";
     export default {
         name: "Reports",
         components: {
-            VueEditor,
+            ReportMessages,
+            EditReport,
             simplebar
         },
         data() {
@@ -285,124 +202,8 @@
         background-color: inherit;
     }
 
-    .chat-message {
-        padding: 10px;
-    }
-
-    .chat {
-        list-style: none;
-        margin: 0;
-        padding: 4px;
-    }
 
 
-
-
-   .chat-body {
-        background-color: #fff;
-        color: #222831;
-    }
-
-    .chat li .chat-body {
-        position: relative;
-        font-size: 11px;
-        border: 1px solid #f1f5fc;
-        box-shadow: 0 1px 1px rgba(0, 0, 0, .05);
-        -moz-box-shadow: 0 1px 1px rgba(0, 0, 0, .05);
-        -webkit-box-shadow: 0 1px 1px rgba(0, 0, 0, .05);
-    }
-
-    .chat li .chat-body .header {
-        border-bottom: 1px solid #f1f5fc;
-    }
-
-
-    .chat li.left .chat-body:before {
-        position: absolute;
-        top: 8px;
-        left: -11px;
-        display: inline-block;
-        background: #fff;
-        width: 16px;
-        height: 16px;
-        border-top: 1px solid #f1f5fc;
-        border-left: 1px solid #f1f5fc;
-        content: '';
-        transform: rotate(-45deg);
-        -webkit-transform: rotate(-45deg);
-        -moz-transform: rotate(-45deg);
-        -ms-transform: rotate(-45deg);
-        -o-transform: rotate(-45deg);
-    }
-
-    .chat li.right .chat-body:before {
-        position: absolute;
-        top: 8px;
-        right: -11px;
-        display: inline-block;
-        background: #fff;
-        width: 16px;
-        height: 16px;
-        border-top: 1px solid #f1f5fc;
-        border-right: 1px solid #f1f5fc;
-        content: '';
-        transform: rotate(45deg);
-        -webkit-transform: rotate(45deg);
-        -moz-transform: rotate(45deg);
-        -ms-transform: rotate(45deg);
-        -o-transform: rotate(45deg);
-    }
-
-
-
-
-    .primary-font {
-        color: #3c8dbc;
-    }
-
-    .t {
-        display: block;
-        width: 100%;
-        overflow: hidden;
-        resize: none;
-        min-height: 40px;
-        height: auto;
-        line-height: 20px;
-    }
-
-    .t[contenteditable]:empty::before {
-        content: "Write a comment...";
-        color: gray;
-    }
-    .chat-input-links {
-        position: absolute;
-        right: 24px;
-        top: 60%;
-        color: #222831;
-        -webkit-transform: translateY(-50%);
-        transform: translateY(-50%);
-    }
-    .cmt{
-        position: absolute;
-        bottom: 12px;
-    }
-   /deep/ .quillWrapper .ql-snow.ql-toolbar {
-
-        background-color: white;
-    }
-    /deep/ .ql-container{
-        height: 120px;
-    }
-    /deep/ .ql-formats{
-        margin: 0!important;
-    }
-    /deep/ .ql-editor{
-        min-height: 100px!important;
-        font-size: 12px!important;
-    }
-   /deep/ .form-control,.custom-select{
-        font-size: 12px!important;
-    }
     .report:hover{
         background-color: #222831;
         color: white;
